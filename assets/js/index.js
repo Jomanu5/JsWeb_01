77c4a609871cc78e1616fe88e7afd2582d05b65a
Casita <casita@iMac-Pro-de-Casita.local>
2025-04-18 14:22:07 -0400

    Trabajo 1 JS

---
+++ assets/js/index.js
@@ -0,0 +1,33 @@
+precio = 400000
+cantidad = 0
+total = 0 
+
+valorTotal = document.querySelector(".valor-total")
+valorTotal.innerHTML = total
+precioSpan = document.querySelector(".precio-inicial")
+precioSpan.innerHTML = precio 
+cantidadSpan = document.querySelector(".cantidad")
+cantidadSpan.innerHTML = cantidad
+
+
+btnMas = document.querySelector(".btnMas")
+btnMenos = document.querySelector(".btnMenos")
+
+btnMas.addEventListener ('click', () => {
+    cantidad = cantidad + 1
+    cantidadSpan.innerHTML = cantidad
+    valorTotal.innerHTML = precio * cantidad
+})
+
+btnMenos.addEventListener ('click',() => {
+    cantidad = cantidad - 1
+    if (cantidad < 0) {
+        cantidad = 0
+    }
+    cantidadSpan.innerHTML = cantidad
+    valorTotal.innerHTML = precio * cantidad
+
+})
+
+  
+
